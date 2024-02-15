b25950246f1920669db1a4513b3e4601c7df6e88
Alena Zaglumonina <al.zaglumonina@gmail.com>
2024-02-15 16:52:48 +0300

    dist

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,10 @@
-export default defineConfig({
-  base: '/test_kokoc/'
-});
+import { defineConfig } from 'vite'
+
+export default defineConfig(({ command, mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  return {
+    // vite config
+    base: '/test_kokoc/'
+  }
+})
